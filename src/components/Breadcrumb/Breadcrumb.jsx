2d0245037151882b2679cd5521d568da0777e490
vanno23 <vakopapunidze19@gmail.com>
2024-05-10 22:11:59 +0400

    cart

---
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Breadcrumb.css";
+const Breadcrumb = ({ page }) => {
+  return (
+    <div className="Breadcrumb">
+      <div className="Breadcrumb_item">
+        <h4>{page}</h4>
+        <div>
+          <span>Ecommerce</span>
+          <i class="fa-solid fa-angle-right"></i>
+          <span>{page}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Breadcrumb;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,72 +2,106 @@ import React from "react";
 import { useCart } from "../useContext/CartContext";
 import "./Cart.css";
 import CartVoice from "../voice/CartVoice";
+import OrderSummary from "../components/OrderSummary/OrderSummary";
+import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
 
 const Cart = () => {
-  const { cart } = useCart();
+  const { cart, setCart } = useCart();
 
   const removeFromCart = (name) => {
-    console.log(`Removing ${name} from cart`);
+    const lowerCaseName = name.toLowerCase();
+    setCart((prevCart) => {
+      const updatedCart = prevCart.filter(
+        (item) => item.name.toLowerCase() !== lowerCaseName
+      );
+      return updatedCart;
+    });
   };
 
   const increaseQuantity = (name) => {
-    // Logic to increase quantity of item in cart
-    console.log(`Increasing quantity of ${name}`);
+    setCart((prevCart) => {
+      const updatedCart = prevCart.map((item) => {
+        if (item.name.toLowerCase() === name.toLowerCase()) {
+          return { ...item, quantity: item.quantity + 1 };
+        }
+        return item;
+      });
+      return updatedCart;
+    });
   };
 
   const decreaseQuantity = (name) => {
-    // Logic to decrease quantity of item in cart
-    console.log(`Decreasing quantity of ${name}`);
+    setCart((prevCart) => {
+      const updatedCart = prevCart.map((item) => {
+        if (item.name.toLowerCase() === name.toLowerCase()) {
+          const newQuantity = Math.max(item.quantity - 1, 1);
+          return { ...item, quantity: newQuantity };
+        }
+        return item;
+      });
+      return updatedCart;
+    });
   };
 
   return (
-    <div className="cartpage">
-      {cart.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <div>
-          <h3>Your Cart</h3>
-          <ul className="cartInfo">
-            {cart.map((item) => (
-              <li key={item.id} className="cartLIst">
-                <div className="cartImage">
-                  <img src={item.image} alt={item.name} />
-                </div>
-                <div>
-                  <h4>{item.name}</h4>
-                  <div className="cartAttribute">
-                    <div className="cartAttributeChild">
-                      <p>Color: </p>
-                      <div
-                        className="cartColor"
-                        style={{ backgroundColor: item.selectedColor }}
-                      ></div>
+    <>
+      <Breadcrumb page={"Cart"} />
+      <div className="cartpage">
+        {cart.length === 0 ? (
+          <p>Your cart is empty.</p>
+        ) : (
+          <div className="cartContainer">
+            <div>
+              <h3 className="cartContainer_title">Your Cart</h3>
+              <ul className="cartInfo">
+                {cart.map((item) => (
+                  <li key={item.id} className="cartLIst">
+                    <div className="cartListLeft">
+                      <div className="cartImage">
+                        <img src={item.image} alt={item.name} />
+                      </div>
+                      <div>
+                        <h4 className="cart_item_name">{item.name}</h4>
+                        <div className="cartAttribute">
+                          <div className="cartAttributeChild">
+                            <p className="cart_item_color">Color: </p>
+                            <div
+                              className="cartColor"
+                              style={{ backgroundColor: item.selectedColor }}
+                            ></div>
+                          </div>
+                          <p>-</p>
+                          <div className="cartAttributeChild">
+                            <p className="cartSize">Size: </p>
+                            <div className="cartSizeData">
+                              {item.selectedSize}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <p>-</p>
-                    <div className="cartAttributeChild">
-                      <p className="cartSize">Size: </p>
-                      <div>{item.selectedSize}</div>
+                    <div className="cartPrice">
+                      <p>${item.price.toFixed(2)}</p>
+                      <p>{item.quantity}</p>
+                      <button onClick={() => removeFromCart(item.name)}>
+                        Remove
+                      </button>
                     </div>
-                  </div>
-                </div>
-                <div className="cartPrice">
-                  <p>Price: ${item.price.toFixed(2)}</p>
-                  <p>{item.quantity}</p>
-                  <button onClick={() => removeFromCart(item.name)}>
-                    Remove
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <CartVoice
-        removeFromCart={removeFromCart}
-        increaseQuantity={increaseQuantity}
-        decreaseQuantity={decreaseQuantity}
-      />
-    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <OrderSummary />
+          </div>
+        )}
+        <CartVoice
+          cart={cart}
+          removeFromCart={removeFromCart}
+          increaseQuantity={increaseQuantity}
+          decreaseQuantity={decreaseQuantity}
+        />
+      </div>
+    </>
   );
 };
 
